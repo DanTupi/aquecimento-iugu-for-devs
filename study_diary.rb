f7e592b42413e87eae6571c3832c1f104e9f5fe1
DanTupi <danilotupinamba@hotmail.com>
2021-03-03 14:29:31 -0300

    refactoring\ functions

--- study_diary.rb
+++ study_diary.rb
@@ -1,3 +1,18 @@
+def clear
+  system('clear')
+end
+  
+def wait_keypress
+  puts
+  puts 'Presione enter para continuar'
+  gets
+end
+
+def wait_keypress_and_wait
+  wait_keypress
+  clear
+end
+
 def welcome
   'Bem-vindo ao Diário de Estudos, seu companheiro para estudar!'
 end
@@ -19,31 +34,44 @@ def register_study_item
   puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
   { title: title, category: category }
 end
+def print_items(collection)
+  collection.each_with_index do |item, index|
+    puts "##{index + 1} - #{item[:title]} - #{item[:category]}"
+  end
+  puts 'Nenhum item cadastrado' if collection.empty?
+end
 
+def search_items(collection)
+  print 'Digite uma palavra para procurar: '
+  term = gets.chomp
+  found_items = collection.filter do |item|
+    item[:title].include?(term)
+  end
+  print_items(found_items)
+  puts 'Nenhum item encontrado' if collection.empty?
+end
+
+clear
 puts welcome
 study_items = []
 option = menu
 
-while option != 4
-  if option == 1
+loop do
+  clear
+  case option
+  when 1
     study_items << register_study_item
-  elsif option == 2
-    study_items.each_with_index do |item, index|
-      puts "##{index + 1} - #{item[:title]} - #{item[:category]}"
-    end
-    puts 'Nenhum item cadastrado' if study_items.empty?
-  elsif option == 3
-    print 'Digite uma palavra para procurar: '
-    term = gets.chomp
-    found_items = study_items.filter do |item|
-      item[:title].include? term
-    end
-    puts found_items
-    puts 'Nenhum item encontrado' if found_items.empty?
+  when 2
+    print_items(study_items)
+  when 3
+    search_items(study_items)
+  when 4
+    clear
+    puts 'Obrigado por usar o Diário de Estudos'
+    break
   else
     puts 'Opção inválida'
   end
+  wait_keypress_and_wait
   option = menu
 end
-
-puts 'Obrigado por usar o Diário de Estudos'  
